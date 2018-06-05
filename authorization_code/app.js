f89103adb92d50e6f85ffa9463801322c0c630ea
Genevieve Schultz <genschultz@gmail.com>
2018-06-05 16:11:13 -0700

    updated spotify api files

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '453369c894b445ec98600309f0fc99a2'; // Your client id
+var client_secret = '964b64e73215470f8defc3a776a3504f'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
